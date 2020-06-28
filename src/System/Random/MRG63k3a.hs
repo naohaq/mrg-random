ba85576020b297cfdfc30357d0648b8761830acb
Naoyuki MORITA <naohaq@ps.sakura.ne.jp>
2020-06-28 17:33:24 +0900

    Create MRG63k3a.hs (empty).

---
+++ src/System/Random/MRG63k3a.hs
@@ -0,0 +1,12 @@
+{- -*- mode: haskell; coding: utf-8-unix -*-  -}
+{-# LANGUAGE BangPatterns, CPP, DeriveDataTypeable, FlexibleContexts,
+    ScopedTypeVariables, TypeFamilies #-}
+
+module System.Random.MRG63k3a
+    (
+    ) where
+
+
+
+
+-- EOF
