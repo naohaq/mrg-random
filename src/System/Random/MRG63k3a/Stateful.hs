5f44d880356c6970eec4f1ba8e569260eb28b61d
Naoyuki MORITA <naohaq@ps.sakura.ne.jp>
2020-07-08 13:46:49 +0900

    Add System.Random.MRG63k3a.Stateful (stub)

---
+++ src/System/Random/MRG63k3a/Stateful.hs
@@ -0,0 +1,23 @@
+{- -*- mode: haskell; coding: utf-8-unix -*-  -}
+{-# LANGUAGE BangPatterns, DeriveDataTypeable, FlexibleContexts,
+    FlexibleInstances, MultiParamTypeClasses, ScopedTypeVariables,
+    TypeFamilies #-}
+
+module System.Random.MRG63k3a.Stateful
+    (
+    ) where
+
+-- import Control.Monad.Primitive (PrimMonad, PrimState, unsafeSTToPrim)
+-- import qualified Data.Primitive.PrimArray as P
+-- import Data.Typeable           (Typeable)
+-- import Data.Word
+-- import Data.Bits               ((.&.),(.|.),shiftL)
+
+-- import System.Random.MRG63k3a.Internal
+
+-- import System.Random.Stateful
+
+
+
+
+-- EOF
