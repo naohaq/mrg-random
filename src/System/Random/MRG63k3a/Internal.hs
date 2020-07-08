58e8680b2f15526b14522c3bad209b65cd968946
MORITA Naoyuki <naohaq@koishi.jitaku>
2020-07-08 12:24:53 +0900

    Add MRG63k3a.Internal module(empty).

---
+++ src/System/Random/MRG63k3a/Internal.hs
@@ -0,0 +1,17 @@
+{- -*- mode: haskell; coding: utf-8-unix -*-  -}
+{-# LANGUAGE BangPatterns #-}
+
+module System.Random.MRG63k3a.Internal
+    (
+    ) where
+
+{-
+import System.Random.MRG.Internal
+
+import Data.Int  (Int64)
+import Data.Word (Word64)
+import Data.List (unfoldr)
+ -}
+
+
+-- EOF
